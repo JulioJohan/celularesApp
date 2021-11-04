6d29dbae12e7aeca22707fd4b2b5889bfd1ede60
JulioJohan <juliojohan10@gmail.com>
2021-11-04 08:26:05 -0600

    Se inicia el proyecto

---
+++ src/app/celulares/pages/celular/celular.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-celular',
+  templateUrl: './celular.component.html',
+  styles: [
+  ]
+})
+export class CelularComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
